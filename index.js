213286fded4c1cce5e0ba53d2ca59da6b134c2e6
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-06-25 23:40:16 +0300

    functions lab 2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+saturdayFun('bathe my dog')
+function saturdayFun( activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`}
+
+const mondayWork = function(activity = 'go to the office') {
+    return  `This Monday, I will ${activity}.`
+
+}
+mondayWork("work from home")
+
+function wrapAdjective(character = "*") {
+    return function(type = "special") {
+        return `You are ${character}${type}${character}!`
+    }
+}
+
+wrapAdjective("*")("a hard worker")
+wrapAdjective("||")("a dedicated programmer")
+
+
